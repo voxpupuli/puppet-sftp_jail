d14d0d6909e52b10716bb2e58452fb9fa69a2187
fmacias <fmacias@ck-d9.pixelxen.secure>
2018-03-01 16:43:38 +0100

    It creates user and jail resources. Also, it creates the chroot folder with a command, because this path may need to be created recursively.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,18 +6,25 @@
 # Parameters
 # ----------
 #
-# * `sample parameter`
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# * `chroot_base: 
+#   Absolute path for chroot.By default, it uses /chroot. To set another, just write in hiera: sftp_jail::chroot_base: '/var/anotherchroot'
+#   Don't type the back stash
 #
 class sftp_jail (
   Stdlib::Absolutepath $chroot_base = '/chroot',
 ) inherits ::sftp_jail::params {
 
-  file { $chroot_base:
+# To make sure it creates recursively the directory
+  exec { "Create ${chroot_base}":
+    creates => $chroot_base,
+    command => "/bin/mkdir -p ${chroot_base}",
+  } -> file { $chroot_base: #}
+#  file { $chroot_base:
     ensure => 'directory',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
+create_resources('sftp_jail::user', lookup('sftp_jail::user', {merge => 'deep'}))
+create_resources('sftp_jail::jail', lookup('sftp_jail::jail', {merge => 'deep'}))
 }
