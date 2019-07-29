27705b0c006c41ec611033d4fb9a44a8d58a2740
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-07-29 20:43:52 +0200

    Fix the tests

--- spec/defines/sftp_jail_user_spec.rb
+++ spec/defines/sftp_jail_user_spec.rb
@@ -1,20 +1,8 @@
 require 'spec_helper'
 
-describe 'sftp_jail::user', type: :define do
+describe 'sftp_jail::user' do
   let :pre_condition do
     'include ssh'
-  end
-  let :facts do
-    {
-      osfamily: 'Debian',
-      operatingsystem: 'Debian',
-      operatingsystemrelease: '6',
-      lsbdistcodename: 'squeeze',
-      id: 'root',
-      concat_basedir: '/dne',
-      path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      kernel: 'Linux'
-    }
   end
   let :title do
     'testuser'
@@ -25,11 +13,20 @@ describe 'sftp_jail::user', type: :define do
     }
   end
 
-  it { is_expected.to compile }
-  it 'creates users homedir' do
-    is_expected.to contain_file('/chroot/testjail/home/testuser').with('ensure' => 'directory',
-                                                                       'owner' => 'testuser',
-                                                                       'group' => 'testuser',
-                                                                       'mode' => '0755')
+  # We need Facter 3 facts - it breaks with version 2
+  on_supported_os(facterversion: '3.0.0').each do |os, facts|
+    context "on #{os}" do
+      let :facts do
+        facts
+      end
+
+      it { is_expected.to compile }
+      it 'creates users homedir' do
+        is_expected.to contain_file('/chroot/testjail/home/testuser').with('ensure' => 'directory',
+                                                                           'owner' => 'testuser',
+                                                                           'group' => 'testuser',
+                                                                           'mode' => '0755')
+      end
+    end
   end
 end
